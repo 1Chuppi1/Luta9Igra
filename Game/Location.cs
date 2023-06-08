84e4a2e861b66441bcc91d5515d019e2d6ecf36e
1Chuppi1 <133848692+1Chuppi1@users.noreply.github.com>
2023-06-08 15:59:50 +0300

    dada

---
+++ Game/Location.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Location
+    {
+
+        string _name;
+        List<Monster> _monsters;
+        Boss _finalboss;
+
+        public Location(string name, List<Monster> monsters, Boss finalboss)
+        {
+            _name = name;
+            _monsters = monsters;
+            _finalboss = finalboss;
+        }
+        public string name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public List<Monster> monsters
+        {
+            get => _monsters;
+            set => _monsters = value;
+        }
+
+        public Boss finalboss
+        {
+            get => _finalboss;
+            set => _finalboss = value;
+        }
+    }
+}
---
+++ Game/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Player
+    {
+        string _name;
+        double _damage;
+        double _hp;
+        double _def;
+
+        public Player(string name, double damage, double hp, double def)
+        {
+            _name = name;
+            _damage = damage;
+            _hp = hp;
+            _def = def;
+        }
+
+        public string name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public double damage
+        {
+            get => _damage;
+            set => _damage = value;
+        }
+        public double hp
+        {
+            get => _hp;
+            set => _hp = value;
+        }
+        public double def
+        {
+            get => _def;
+            set => _def = value;
+        }
+    }
+}
